e3ef1c301332a37da0ed8c06d6ffb437046925ff
Docartes <ahmadv0101@gmail.com>
2024-01-20 10:20:31 +0700

    FreeCodeCamp - Timestamp Microservice

--- index.js
+++ index.js
@@ -15,18 +15,55 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+	res.json({greeting: 'hello API'});
 });
+
+app.get('/api', (req, res) => {
+	const date = new Date()
+	const unix = date.getTime();
+
+	res.send({
+		unix: unix, 
+		date: date.toGMTString()
+	})
+})
+
+app.get('/api/:date', (req, res) => {
+	const { date } = req.params;
+	let tanggal = new Date(date)
+	console.log(tanggal)
+	let unix = tanggal.getTime()
+
+	if (tanggal == 'Invalid Date') {
+		res.send({
+			error: 'Invalid Date'
+		})
+	} else {
+		if (parseInt(date).toString().length !== 4) {
+			let t = new Date(parseInt(date))
+			res.send({
+				unix: date,
+				date: t.toGMTString()
+			})
+		} else {
+			res.send({
+				unix: unix,
+				date: tanggal.toGMTString()
+			})
+		}
+	}
+	
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000 || process.env.PORT, function () {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
